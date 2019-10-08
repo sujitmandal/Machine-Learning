16b03794c2ad54cd956012e713636d976d8534c6
Sujit Mandal <49348037+sujitmandal@users.noreply.github.com>
2019-10-08 15:39:16 +0530

    Update hsv_full.py

--- threshold/hsv_full.py
+++ threshold/hsv_full.py
@@ -15,46 +15,15 @@ This programe is create by Sujit Mandal
 
 """
 
-image_1 = cv2.imread('D:\\Matchin Larning\\all dataset\\LISC Database\\Main Dataset\\mixt\\1.bmp')
-#image = cv2.cvtColor(image_1, cv2.COLOR_BGR2GRAY)
-image = cv2.cvtColor(image_1, cv2.COLOR_RGB2HSV_FULL)
+img = cv2.imread('D:\\Matchin Larning\\all dataset\\LISC Database\\Main Dataset\\mixt\\1.bmp')
 
-#plt.title("Original")
-plt.imshow(image)
-#plt.show()
+rgb2hsv_full = cv2.cvtColor(img, cv2.COLOR_RGB2HSV_FULL)
 
-# Compute the Laplacian of the image
-lap = cv2.Laplacian(image, cv2.CV_64F)
-lap_1 = np.uint8(np.absolute(lap))
 
-#plt.title("Laplacian")
-plt.imshow(lap)
-#plt.show()
-plt.imshow(lap_1)
-#plt.show()
-
-# Compute gradients along the X and Y axis, respectively
-sobel_X = cv2.Sobel(image, cv2.CV_64F, 1, 0, ksize=3)
-sobel_Y = cv2.Sobel(image, cv2.CV_64F, 0, 1, ksize=3)
-
-sobel_X = np.uint8(np.absolute(sobel_X))
-sobel_Y = np.uint8(np.absolute(sobel_Y))
-
-#plt.title("Sobel X")
-plt.imshow(sobel_X)
-#plt.show()
-
-#plt.title("Sobel Y")
-plt.imshow(sobel_Y)
-#plt.show()
-
-sobelCombined = cv2.bitwise_or(sobel_X, sobel_Y)
 plt.title("HSV_FULL")
-plt.imshow(sobelCombined)
-plt.show()
+plt.imshow(rgb2hsv_full)
 
-#canny = cv2.Canny(sobelCombined, 30, 150)
-#cv2.imshow("Canny", canny)
+plt.show()
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
