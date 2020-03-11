896815885bd94318487e405742a20f0d7aa5c7f5
Sujit Mandal <49348037+sujitmandal@users.noreply.github.com>
2020-03-11 22:19:38 +0530

    Update lasso_regression.py

--- Regression/Lasso Regression/lasso_regression.py
+++ Regression/Lasso Regression/lasso_regression.py
@@ -1,9 +1,19 @@
+# Import required libraries
 import numpy as np
 import pandas as pd 
+from numpy import std
+from numpy import mean
+from math import sqrt
 import matplotlib.pyplot as plt
 from sklearn import linear_model
 from scipy.stats import spearmanr
 from sklearn.metrics import r2_score
+from sklearn.metrics import max_error
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import median_absolute_error
+from sklearn.metrics import mean_squared_log_error
+
 
 #Github: https://github.com/sujitmandal
 
@@ -34,12 +44,14 @@ x = data['YearsExperience'].values.reshape(-1,1)
 y = data['Salary'].values.reshape(-1,1)
 
 xnew = x[20:30]
+ynew = y[20:30]
 x = x[:20]
 y = y[:20]
 
 #Data Visualition After Processing
 print('\n')
 print('xnew:',xnew.shape)
+print('ynew:',ynew.shape)
 print('x:',x.shape)
 print('y:',y.shape)
 
@@ -50,40 +62,102 @@ plt.ylabel('Salary')
 plt.scatter(x,y)
 plt.show()
 
+
+x_mean = mean(x)
+x_stdv = std(x)
+
+y_mean = mean(y)
+y_stdv = std(y)
+
+print('\n')
+print('X Mean = %0.3f' % x_mean)
+print('X Standard Deviation = %0.3f' %x_stdv)
+print('\n')
+print('Y Mean = %0.3f' % y_mean)
+print('Y Standard Deviation = %0.3f' %y_stdv)
+
 #Spearman's Correlation
 correlation, _ = spearmanr(x, y)
 print('\n')
 print('Spearmans correlation: %.5f' % correlation)
-print('\n')
 
 #Regression Model
 lr = linear_model.Lasso(alpha=1.0).fit(x, y)
 print('\n')
 print(lr)
 
-score = lr.score(x,y)
-print('\n')
-print('Score: %.5f' % score)
-
-#R^2 (coefficient of determination)
-r2_Score = r2_score(x, y)
-print('\n')
-print('r2 Score : %.5f' % r2_Score)
-
 intercept = (lr.intercept_)
 print('\n')
-print('Intercept:')
-intercepts = intercept.reshape(-1, 1)
-print(intercepts)
+print('Intercept: %.5f' % intercept)
 
 #Prediction
 predict = lr.predict(xnew)
 print('\n')
-predicted = predict.reshape(-1,1)
 print('Prediction:')
-print(predicted)
+print(predict)
 
-#OUTPUT:
+x_true = xnew
+y_true = ynew
+y_pred = predict
+
+score = lr.score(y_true, y_pred)
+print('\n')
+print('Score: %.5f' % score)
+
+#Coefficients
+coef = (lr.coef_)
+print('Coefficients: ', coef)
+
+#R^2 (coefficient of determination)
+r2_Score = r2_score(y_true, y_pred)
+print('r2 Score : %.5f' % r2_Score)
+
+#Root Mean Squared Error
+rmse = sqrt(mean_squared_error(y_true, y_pred))
+print('\n')
+print('Model Result :')
+print('Root Mean Squared Error = %0.3f' % rmse)
+
+#Mean Squared Error
+mse = mean_squared_error(y_true, y_pred)
+print('Mean Squared Error = %0.3f' % mse)
+
+#Mean Absolute Error
+mae = mean_absolute_error(y_true, y_pred)
+print('Mean Absolute Error = %0.3f' % mae)
+
+#Median Absolute Error
+med_ea = median_absolute_error(y_true, y_pred)
+print('Median Absolute Error = %0.3f' % med_ea)
+
+#Mean Squared Log Error
+msle = mean_squared_log_error(y_true, y_pred)
+print('Mean Squared Log Error = %0.3f' % msle)
+
+#Max Error
+me = max_error(y_true, y_pred)
+print('Max Error = %0.3f' % me)
+
+#Polt Actual vs. Predicted
+plt.title('Actual vs. Predicted')
+plt.xlabel('YearsExperience')
+plt.ylabel('Salary')
+plt.scatter(x_true, y_true)
+plt.scatter(x_true, y_pred)
+plt.show()
+
+#Outputs Plot
+plt.title('Actual vs. Predicted')
+plt.xlabel('YearsExperience')
+plt.ylabel('Salary')
+plt.scatter(x_true, y_true)
+plt.scatter(x_true, y_pred, color='r')
+plt.plot(x_true, y_pred, color='y', linewidth=3)
+plt.xticks(())
+plt.yticks(())
+plt.show()
+
+#OUTPUT :
 '''
    YearsExperience   Salary
 0              1.1  39343.0
@@ -105,13 +179,20 @@ print(predicted)
 
 
 xnew: (10, 1)
+ynew: (10, 1)
 x: (20, 1)
 y: (20, 1)
 
 
+X Mean = 3.590
+X Standard Deviation = 1.432
+
+
+Y Mean = 59304.250
+Y Standard Deviation = 14381.643
+
+
 Spearmans correlation: 0.87058
-
-
 
 
 Lasso(alpha=1.0, copy_X=True, fit_intercept=True, max_iter=1000,
@@ -119,25 +200,25 @@ Lasso(alpha=1.0, copy_X=True, fit_intercept=True, max_iter=1000,
       selection='cyclic', tol=0.0001, warm_start=False)
 
 
-Score: 0.82364
-
-
-r2 Score : -1816363021.30743
-
-
-Intercept:
-[[26579.15132445]]
+Intercept: 26579.15132
 
 
 Prediction:
-[[ 88565.41065418]
- [ 91300.09856578]
- [ 98592.5996634 ]
- [101327.287575  ]
- [105885.10076101]
- [108619.78867262]
- [113177.60185863]
- [114089.16449583]
- [120470.10295624]
- [122293.22823065]]
+[ 88565.41065418  91300.09856578  98592.5996634  101327.287575
+ 105885.10076101 108619.78867262 113177.60185863 114089.16449583
+ 120470.10295624 122293.22823065]
+
+
+Score: -8395486220.93957
+Coefficients:  [9115.62637202]
+r2 Score : 0.71529
+
+
+Model Result :
+Root Mean Squared Error = 5138.616
+Mean Squared Error = 26405370.644
+Mean Absolute Error = 3951.098
+Median Absolute Error = 3105.189
+Mean Squared Log Error = 0.002
+Max Error = 12484.712
 '''
